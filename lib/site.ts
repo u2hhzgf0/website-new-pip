9b810ffaa6262c8ee33774cad1152a44add1b999
unknown <codervaidev@gmail.com>
2026-04-12 18:22:51 +0600

    added a icon

---
+++ lib/site.ts
@@ -0,0 +1,3 @@
+/** Tab / brand image (also used in `app/layout.tsx` metadata icons) */
+export const SITE_LOGO_URL =
+  'https://res.cloudinary.com/dshkbza19/image/upload/v1775996191/WhatsApp_Image_2026-04-09_at_7.37.00_PM_yfqkei.jpg'
