0a27bb8c04318f039e4a2d79936627bdd52886eb
u2hhzgf0 <u2hhzgf0@gmail.com>
2026-04-14 09:54:38 +0600

    added some pai

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,13 +15,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: 'https://res.cloudinary.com/dshkbza19/image/upload/v1775996191/WhatsApp_Image_2026-04-09_at_7.37.00_PM_yfqkei.jpg',
         sizes: '192x192',
         type: 'image/jpeg',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
       {
         src: 'https://res.cloudinary.com/dshkbza19/image/upload/v1775996191/WhatsApp_Image_2026-04-09_at_7.37.00_PM_yfqkei.jpg',
         sizes: '512x512',
         type: 'image/jpeg',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
     ],
   }
